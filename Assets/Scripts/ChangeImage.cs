ffe1acf9e34144e5abe247b697edfbb63a8e6338
v012345 <v138468@gmail.com>
2023-08-10 17:45:24 +0800

    加一退出逻辑

--- Assets/Scripts/ChangeImage.cs
+++ Assets/Scripts/ChangeImage.cs
@@ -6,20 +6,30 @@ using UnityEngine.UI;
 public class ChangeImage : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int b = 0;
     void Start()
     {
-               this.GetComponent<Button>().onClick.AddListener
-            (delegate()
-            {
-                OnClick_close(this.gameObject);
-            });
+        this.GetComponent<Button>().onClick.AddListener(delegate(){
+            OnClick_close(this.gameObject);
+        });
     }
        public void OnClick_close(GameObject _obj)
     {
+        if(b < 2){
+
+        GameObject.Find("RawImage").SetActive(false);
+        }else{
+          #if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+ #else
+         Application.Quit();
+#endif
+        }
+        this.b++;
+
         print("点击了按钮："+_obj.name);
+
     }
-
-
     // Update is called once per frame
     void Update()
     {
